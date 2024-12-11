96c1ace2e672f3fcf6dadc24ea1b9705b60afd36
aaccccdddeeffgg <wangyanqi602.gmail.com>
2024-12-11 21:23:15 +0800

    first commit

---
+++ Gemma_finetune.py
@@ -0,0 +1,172 @@
+
+"""
+需要的函数库
+pip install "tensorflow[and-cuda]"
+pip install -U keras-nlp
+pip install -U keras
+pip install tensorrt
+pip install huggingface_hub # Optional if you want to push to Hugging Face
+huggingface-cli login # Optional
+wget -O databricks-dolly-15k.jsonl https://huggingface.co/datasets/databricks/databricks-dolly-15k/resolve/main/databricks-dolly-15k.jsonl
+"""
+
+
+
+import os
+import json
+import keras
+import keras_nlp
+
+os.environ["KAGGLE_USERNAME"] = os.environ["wyq1234597"]
+os.environ["KAGGLE_KEY"] = os.environ["xm23666666"]
+
+os.environ["KERAS_BACKEND"] = "torch"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "1.00"
+
+data = []
+with open("databricks-dolly-15k.jsonl") as file:
+    for line in file:
+        features = json.loads(line)
+        if features["context"]:
+            continue
+        template = "Instruction:\n{instruction}\n\nResponse:\n{response}"
+        data.append(template.format(**features))
+
+data = data[:100]
+
+gemma_lm = keras_nlp.models.GemmaCausalLM.from_preset("gemma_2b_en")
+gemma_lm.summary()
+
+print("Before fine-tuning:\n\n")
+
+prompt = template.format(
+    instruction="What should I do on a trip to Europe?",
+    response="",
+)
+print(gemma_lm.generate(prompt, max_length=256))
+
+prompt = template.format(
+    instruction="Explain the process of photosynthesis in a way that a child could understand.",
+    response="",
+)
+print(gemma_lm.generate(prompt, max_length=256))
+
+gemma_lm.backbone.enable_lora(rank=4)
+gemma_lm.summary()
+
+gemma_lm.preprocessor.sequence_length = 512
+optimizer = keras.optimizers.AdamW(
+    learning_rate=5e-5,
+    weight_decay=0.01,
+)
+optimizer.exclude_from_weight_decay(var_names=["bias", "scale"])
+
+gemma_lm.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=optimizer,
+    weighted_metrics=[keras.metrics.SparseCategoricalAccuracy()],
+)
+gemma_lm.fit(data, epochs=1, batch_size=1)
+
+gemma_lm.save('finetuned_model.keras')
+
+print("After fine-tuning:\n")
+
+prompt = template.format(
+    instruction="What should I do on a trip to Europe?",
+    response="",
+)
+print(gemma_lm.generate(prompt, max_length=256))
+
+prompt = template.format(
+    instruction="Explain the process of photosynthesis in a way that a child could understand.",
+    response="",
+)
+print(gemma_lm.generate(prompt, max_length=256))
+
+# import os
+# import json
+# import keras
+# import keras_nlp
+#
+# os.environ["KAGGLE_USERNAME"] = os.environ["KAGGLE_USERNAME"]
+# os.environ["KAGGLE_KEY"] = os.environ["KAGGLE_KEY"]
+#
+# os.environ["KERAS_BACKEND"] = "torch"
+# os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "1.00"
+#
+# data = []
+# with open("databricks-dolly-15k.jsonl") as file:
+#     for line in file:
+#         features = json.loads(line)
+#         if features["context"]:
+#             continue
+#         template = "Instruction:\n{instruction}\n\nResponse:\n{response}"
+#         data.append(template.format(**features))
+#
+# data = data[:1000]
+#
+# gemma_lm = keras_nlp.models.GemmaCausalLM.from_preset("gemma_2b_en")
+# gemma_lm.summary()
+#
+# print("Before fine-tuning:\n\n")
+#
+# prompt = template.format(
+#     instruction="What should I do on a trip to Europe?",
+#     response="",
+# )
+# print(gemma_lm.generate(prompt, max_length=256))
+#
+# prompt = template.format(
+#     instruction="Explain the process of photosynthesis in a way that a child could understand.",
+#     response="",
+# )
+# print(gemma_lm.generate(prompt, max_length=256))
+#
+# gemma_lm.backbone.enable_lora(rank=4)
+# gemma_lm.summary()
+#
+# gemma_lm.preprocessor.sequence_length = 512
+# optimizer = keras.optimizers.AdamW(
+#     learning_rate=5e-5,
+#     weight_decay=0.01,
+# )
+# optimizer.exclude_from_weight_decay(var_names=["bias", "scale"])
+#
+# gemma_lm.compile(
+#     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#     optimizer=optimizer,
+#     weighted_metrics=[keras.metrics.SparseCategoricalAccuracy()],
+# )
+# gemma_lm.fit(data, epochs=1, batch_size=1)
+#
+# model_save_kwargs = {
+#     "save_format": "tf",  # Specifies to save in TensorFlow format; alternatives include 'h5' for HDF5
+#     "include_optimizer": True,  # Whether to save the optimizer's state as well
+# }
+#
+# gemma_lm.save('finetuned_model.keras', save_format="tf", include_optimizer=True)
+#
+# # Now, when pushing to the Hugging Face Hub
+# from huggingface_hub import push_to_hub_keras
+#
+# push_to_hub_keras(
+#     gemma_lm,
+#     "praison/gemma-2b-en-finetuned-databricks-dolly-15k",
+#     tags=["gemma-2b-en", "finetuned", "databricks-dolly-15k", "gemma", "lora"],
+#     **model_save_kwargs  # This expands to fill in the save arguments for the model
+# )
+#
+# print("After fine-tuning:\n")
+#
+# prompt = template.format(
+#     instruction="What should I do on a trip to Europe?",
+#     response="",
+# )
+# print(gemma_lm.generate(prompt, max_length=256))
+#
+# prompt = template.format(
+#     instruction="Explain the process of photosynthesis in a way that a child could understand.",
+#     response="",
+# )
+# print(gemma_lm.generate(prompt, max_length=256))
